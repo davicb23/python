a4ce1e08edad54311d99dba6b2e9115883215b9a
Davi Brito <davibritocontato@gmail.com>
2025-01-14 18:00:12 -0300

    4 novos exercícios realizados

---
+++ atividades/ex005.py
@@ -0,0 +1,6 @@
+n= int(input('Digite um número: '))
+ant= n - 1
+suc= n + 1
+
+print('Analisando o valor {}, o seu antecessor é {}, e seu sucessor é {}' .format(n, ant, suc))
+
---
+++ atividades/ex008.py
@@ -0,0 +1,10 @@
+m = float(input('Digite uma distância em metros: '))
+km = m * 1000
+hm =  m * 100
+dam = m * 10
+dm = m / 10
+cm = m / 100
+mm = m / 1000
+print('{}m em: ' .format(m))
+print('KM = {} \nHM = {} \nDAM = {}' .format(km, hm, dam))
+print('DM = {} \nCM = {} \nMM = {}' .format(dm, cm, mm))
