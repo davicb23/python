12af655835b689c0aa382265b237f230ed417481
Davi Brito <davibritocontato@gmail.com>
2025-04-08 19:18:28 -0300

    .

--- atividades/ex019.py
+++ atividades/ex019.py
@@ -1,8 +1,9 @@
-import random
+from random import choice
 n1 = input('Primeiro aluno: ')
 n2 = input('Segundo aluno: ')
 n3 = input('Terceiro aluno: ')
 n4 = input('Quarto aluno: ')
 lista = [n1, n2, n3, n4]
 '''LISTAS FICAM ENTRE []'''
-escolhido = random.choice(lista)
+escolhido = choice(lista)
+print(f'O aluno escolhido foi {escolhido}')
---
+++ atividades/ex019bonus.py
@@ -0,0 +1,8 @@
+'''Jeito mais otimizado'''
+from random import randint
+print(randint(1, 6))
+
+'''from random import choice
+dado = [1, 2, 3, 4, 5, 6]
+n = choice(dado)
+print(n)'''
